31d00a3f97e93467442664880d1884006083b222
Luis Sanchez <lespinerua@gmail.com>
2020-02-06 23:01:04 -0400

    Layout

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -17,8 +17,7 @@ const Home = () => {
   return initialState.length === 0 ? (
     <h1>Loading...</h1>
   ) : (
-    <div className="App">
-      <Header />
+    <>
       <Search />
       {initialState.mylist.length > 0 && (
         <Categories title="Mi lista">
@@ -43,8 +42,7 @@ const Home = () => {
           ))}
         </Carousel>
       </Categories>
-      <Footer />
-    </div>
+    </>
   );
 };
 
