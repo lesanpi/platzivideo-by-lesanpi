8053aaea3eaaa581549cb7502f40ff8e57f42fba
Luis Sanchez <lespinerua@gmail.com>
2020-02-06 22:07:25 -0400

    Agregando react router y HomeRoute

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -11,7 +11,7 @@ import "../assets/styles/App.scss";
 
 const API = "http://localhost:3000/initialState";
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   return initialState.length === 0 ? (
@@ -48,4 +48,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
