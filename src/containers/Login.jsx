f7a9a788a8680561dd63295d61293ea97a5fa01d
Luis Sanchez <lespinerua@gmail.com>
2020-02-12 04:03:58 -0400

    Implementando redux en Login

--- src/containers/Login.jsx
+++ src/containers/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
+import { connect } from "react-redux";
+import { loginRequest } from "../actions";
 import { Link } from "react-router-dom";
 import googleIcon from "../assets/static/google-icon.png";
 import twitterIcon from "../assets/static/twitter-icon.png";
 import "../assets/styles/components/Login.scss";
 
-const Login = () => {
+const Login = props => {
   const [form, setValues] = useState({
     email: ""
   });
@@ -18,7 +20,8 @@ const Login = () => {
 
   const handleSubmit = event => {
     event.preventDefault();
-    console.log(form);
+    props.loginRequest(form);
+    props.history.push("/");
   };
 
   return (
@@ -64,4 +67,9 @@ const Login = () => {
     </section>
   );
 };
-export default Login;
+
+const mapDispatchToProps = {
+  loginRequest
+};
+
+export default connect(null, mapDispatchToProps)(Login);
